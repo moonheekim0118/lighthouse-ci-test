6f8890030112c144657ae930aaa0d21aece5f4d9
moonheekim0118 <hellomooneekim@gmail.com>
2022-08-11 00:59:13 +0900

    feat: 수저엉

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>허허</p>
+        <p>허허. 코멘트 달아주시지요.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
