55896ba03c424007f18d957f4c2bedf8ed7cbc97
moonheekim0118 <hellomooneekim@gmail.com>
2022-08-11 00:04:53 +0900

    feat: 귀여운 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>헬로헬롱</p>
+        <p>헬로헬롱1</p>
         <a
           className="App-link"
           href="https://reactjs.org"
