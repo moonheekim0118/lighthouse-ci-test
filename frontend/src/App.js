cfbe72ad146b9f8fb17c4aa3ed14c43e05497396
moonheekim0118 <hellomooneekim@gmail.com>
2022-08-11 17:03:24 +0900

    fix: 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,22 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>안녕안녕ㅇㅇ</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          움하하
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">아무것도 없다.</div>;
 }
 
 export default App;
