613b9abea87fe365f1350b888b11de7ecce71f5d
moonheekim0118 <hellomooneekim@gmail.com>
2022-08-11 11:42:46 +0900

    fix: 수정ㅠㅠ

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>대대적인 수정2232</p>
+        <p>대대적인 수정221232</p>
         <a
           className="App-link"
           href="https://reactjs.org"
