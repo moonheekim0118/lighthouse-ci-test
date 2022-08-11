b5408295ccb773021d24dbec6431f38419d18c26
moonheekim0118 <hellomooneekim@gmail.com>
2022-08-11 11:12:06 +0900

    feat: 대대적인 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>허허. 코멘트 달아주시지요.</p>
+        <p>대대적인 수정</p>
         <a
           className="App-link"
           href="https://reactjs.org"
