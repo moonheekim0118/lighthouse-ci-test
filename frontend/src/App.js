c5297bf955e5e9e8acd5733339df67770ad7f719
moonheekim0118 <hellomooneekim@gmail.com>
2022-08-11 17:00:38 +0900

    feat: 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>안녕안녕</p>
+        <p>안녕안녕ㅇㅇ</p>
         <a
           className="App-link"
           href="https://reactjs.org"
